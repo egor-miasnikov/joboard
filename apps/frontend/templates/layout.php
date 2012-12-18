35ddd6e1cd2a50154008d6326e5d5bb4a42c8159
Egor Miasnikov <miasnikov.egor@sadgroup.com>
2012-12-18 11:57:13 +0300

    change layout add css

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
@@ -9,6 +8,51 @@
     <?php include_javascripts() ?>
   </head>
   <body>
-    <?php echo $sf_content ?>
+      <div id="container">
+          <div id="header">
+              <div class="content">
+                  <h1>
+                      <a href="/job">Job</a>
+                  </h1>
+                  <div id="sub_header">
+                      <div class="post">
+                          <h2>Ask for people</h2>
+                          <div>
+                              <a class="btn btn-primary" href="/job/new">Add Job</a>
+                          </div>
+                      </div>
+                      <div class="search-query">
+                          <h2>Ask for a job</h2>
+                          <form action="" method="get">
+                              <input type="text" name="keywords" id="search_keywords" />
+                              <input type="submit" value="search" />
+                              <div class="help">Enter some keywords (city, country, position, ...)</div>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          
+          <div id="content">
+              <?php if ($sf_user->hasFlash('notice')): ?>
+              <div class="flash_notice"><?php echo $sf_user->getFlash('notice')?></div>
+              <?php endif; ?>
+              <?php if ($sf_user->hasFlash('error')): ?>
+              <div class="flash_error"><?php echo $sf_user->getFlash('error')?></div>
+              <?php endif; ?>
+              <div class="content"><?php echo $sf_content ?></div>
+          </div>
+          <div id="footer">
+              <div class="content">
+                  <span class="symfony">powered by Symfony 1.4</span>
+                  <ul class="inline">
+                      <li><a href="/about">About Joboard</a></li>
+                      <li class="feed"><a href="/feed">Full feed</a></li>
+                      <li><a href="/api">Joboard API</a></li>
+                      <li class="last"><a href="">Affiliates</a></li>
+                  </ul>
+              </div>
+          </div>
+      </div>    
   </body>
 </html>
